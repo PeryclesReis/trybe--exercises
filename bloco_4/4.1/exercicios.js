f327a75f89fe1a1ba913ffaecc86c68f4e4128b0
PeryclesF <perycles_reis@hotmail.com>
2020-12-17 21:03:40 -0300

    adicionando exercicio 4.1

---
+++ bloco_4/4.1/exercicios.js
@@ -0,0 +1,13 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+let a = 2;
+let b = 5;
+// Adição
+console.log(a + b);
+// Subtração
+console.log(a - b);
+// Multiplicação
+console.log(a * b);
+// Divisão
+console.log(a / b);
+// Módulo
+console.log(a % b);
---
+++ bloco_4/exemplo01.js
@@ -0,0 +1,11 @@
+let notaCandidato = 59;
+
+if (notaCandidato >= 80) {
+
+  console.log('Parabéns, você foi aprovado!');
+
+} else if (notaCandidato >= 60) {
+
+  console.log('Você está na nossa lista de espera!');
+
+} else console.log('Você foi reprovado!');
