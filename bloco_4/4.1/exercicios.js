85cac40d8f0737062ddf85ee6ef0a6bde89ebc49
PeryclesF <perycles_reis@hotmail.com>
2020-12-17 21:10:24 -0300

    segunda questao

--- bloco_4/4.1/exercicios.js
+++ bloco_4/4.1/exercicios.js
@@ -11,3 +11,8 @@ console.log(a * b);
 console.log(a / b);
 // Módulo
 console.log(a % b);
+
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+if (a > b) {
+  console.log('A é maior que B! ' + a);
+} else console.log('B é maior que A! ' + b);
