c3304a573f09a64bb2e94befd4f5a346057b7794
PeryclesF <perycles_reis@hotmail.com>
2020-12-18 21:34:00 -0300

    quarto exercicio

--- bloco_4/4.1/exercicios.js
+++ bloco_4/4.1/exercicios.js
@@ -25,3 +25,11 @@ if (a > b && a > c) {
   return console.log('B é maior que A e C! ' + b);
 } else 
 return console.log('C é maior que B e A! ' + c);
+
+// Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+let valor = 1;
+if (valor > 0) {
+  console.log('positive');
+} else if (valor < 0) {
+  console.log('negative');
+} else console.log('zero');
