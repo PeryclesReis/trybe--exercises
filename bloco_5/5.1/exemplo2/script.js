d3385807ca08b27b59fba60db9459ac0a119a0f8
PeryclesF <perycles_reis@hotmail.com>
2020-12-29 17:45:08 -0300

    praticando dom bloco5

---
+++ bloco_5/5.1/exemplo2/script.js
@@ -0,0 +1,16 @@
+document.querySelector('body').style.backgroundColor = '#f3f3f3';
+
+document.querySelectorAll('header')[0].style.backgroundColor = '#59b16a';
+
+document.querySelectorAll('.emergency-tasks')[0].style.backgroundColor = '#f59d83';
+
+document.querySelectorAll('.emergency-tasks h3')[0].style.backgroundColor = '#ae73f4';
+document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = '#ae73f4';
+
+
+document.querySelectorAll('.no-emergency-tasks')[0].style.backgroundColor = '#f9db5e';
+
+document.querySelectorAll('.no-emergency-tasks h3')[0].style.backgroundColor = '#000';
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.backgroundColor = '#000';
+
+document.querySelectorAll('footer')[0].style.backgroundColor = '#123633';
