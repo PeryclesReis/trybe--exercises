d4b51df3935a7fd6909ef5caaf9322deef6143a7
Perycles floriano <perycles_reis@hotamil.com>
2021-01-27 19:46:45 -0300

    dia 9.1 exercicios

--- bloco_7/7.1/exercicio_2.js
+++ bloco_7/7.1/exercicio_2.js
@@ -31,3 +31,27 @@ const fatorial3 = (n) => n === 0 || n == 1 ? 1 : n * fatorial3(n - 1);
 console.log(fatorial(5));
 console.log(fatorial2(5));
 console.log(fatorial3(5));
+// ---------------------------------------------------------------------------------
+// Crie uma função que receba uma frase e retorne qual a maior palavra.
+
+function WordBiggest(phrase) {
+  let contador = 0;
+  let maiorPalavra = '';
+  const array = phrase.split(' ');
+  array.forEach(element => {
+    if (element.length > contador) {
+      contador = element.length;
+      maiorPalavra = element;
+      return element;
+    }
+  });
+  console.log(maiorPalavra);
+  return maiorPalavra;
+}
+
+WordBiggest('Antônio foi no banheiro e não sabemos o que aconteceu');
+// ------------------------------------------------------------------------------------
+// Crie uma página que contenha:
+// Um botão ao qual será associado um event listener ;
+// Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
+// Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável clickCount é atualizada.
