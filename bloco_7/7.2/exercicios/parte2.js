59413c0090146056211a76b08754572b75a333db
Perycles floriano <perycles_reis@hotamil.com>
2021-01-31 15:54:15 -0300

    parte II dia 7.2

--- bloco_7/7.2/exercicios/parte2.js
+++ bloco_7/7.2/exercicios/parte2.js
@@ -46,26 +46,31 @@ function listValues(obj) {
 // console.log(listValues(lesson3));
 
 // Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
-function allLessons(obj1, obj2, obj3) {
-  const obj = {};
-  obj.lesson1 = obj1;
-  obj.lesson2 = obj2;
-  obj.lesson3 = obj3;
-  const newObj = {};
-  Object.assign(newObj, obj);
-
-  return newObj;
-}
+const allLessons = (obj1, obj2, obj3) => Object.assign({}, {obj1, obj2, obj3});
 console.log(allLessons(lesson1, lesson2, lesson3));
 
 // Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+function studentsTotal(...rest) {
+  let qtdAlunos = 0;
+  rest.forEach((lesson) => {
+    const keys = lesson.numeroEstudantes;
+    qtdAlunos += keys;
+  });
+  return `Total de alunos: ${qtdAlunos}`;
+}
+// console.log(studentsTotal(lesson1, lesson2, lesson3));
 
 // Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
 // console.log(getValueByNumber(lesson1, 0));
 // Output: 'Matématica'
+const getValueByNumber = (obj,number) => Object.values(obj)[number];
+// console.log(getValueByNumber(lesson1, 1));
 
 // Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave. Exemplo:
 // console.log(verifyPair(lesson3, 'turno', 'noite'));
 // // Output: true,
 // console.log(verifyPair(lesson3, 'materia', 'Maria Clara'));
 // // Output: false
+const verifyPair = (obj, key, value) => Object.entries(obj)
+.some((entrie) => entrie[0] === key && entrie[1] === value);
+// console.log(verifyPair(lesson1, 'turno','manhã'));
