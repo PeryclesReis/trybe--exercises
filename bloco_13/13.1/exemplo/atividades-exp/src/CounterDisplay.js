256ee6d17c7974485c88a7318199d8d9ede51dd7
Perycles floriano <perycles_reis@hotamil.com>
2021-03-06 11:35:22 -0300

    bloco 13 dia 1 fixacao

---
+++ bloco_13/13.1/exemplo/atividades-exp/src/CounterDisplay.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CounterDisplay extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.value % 3 !== 0) return false;
+    return true;
+  }
+
+  render() {
+    return <div> counter: {this.props.value}</div>;
+  }
+}
+
+export default CounterDisplay;
